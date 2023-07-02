b02683718408646a4db7ffe657487e70e21f57c4
Moon Patel <moonpatel2003@gmail.com>
2023-07-02 16:10:15 +0530

    TODO list for apis added

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,13 +1,46 @@
 const router = require("express").Router();
-
-const { checkAuth } = require("../util/auth");
 const { User } = require("../models/user");
 
+// TODO
+// get user details
+router.get("/:username", async (req, res, next) => {
+    let username;
+});
+
+// TODO
+// update user details
+router.post("/:username", async (req, res, next) => {});
+
+// get friends of the user
 router.get("/:username/friends", async (req, res, next) => {
     const username = req.params.username;
-    const user = await User.findOne({username});
+    const user = await User.findOne({ username });
     const friends = await user.getFriends();
     return res.json({ success: true, friends });
+});
+
+// TODO
+// add a friend
+router.post("/:username/friends/:friend_username", async (req, res, next) => {
+    res.send("TODO");
+});
+
+// TODO
+// remove a user from friends list
+router.delete("/:username/friends/:friend_username", async (req, res, next) => {
+    res.send("TODO");
+});
+
+// TODO
+// get history of games played
+router.get("/:username/games", async (req, res, next) => {
+    res.send("TODO");
+});
+
+// TODO
+// get a particular game
+router.get("/:username/games/:gameid", async (req, res, next) => {
+    res.send("TODO");
 });
 
 module.exports = router;
